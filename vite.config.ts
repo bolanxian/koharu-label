ecb7657247cdae5f78ac244595898a7228b30287
僠儖僲 <108845763+bolanxian@users.noreply.github.com>
2025-04-09 12:00:00 +0800

    Destroy `vite:build-import-analysis`

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,37 @@ import type { Plugin } from 'vite'
 import { defineConfig, createFilter } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { marked } from 'marked'
-// https://vitejs.dev/config/
+
+const destroyModulePreload = (): Plugin => {
+  const name = 'Destroy `vite:build-import-analysis`'
+  const { exec } = RegExp.prototype, { apply } = Reflect
+  // from vite@6.2.5/dist/node/chunks/dep-Pj_jxEzN.js:50550
+  const target = /((?:\bconst\s+|\blet\s+|\bvar\s+|,\s*)(\{[^{}.=]+\})\s*=\s*await\s+import\([^)]+\))|(\(\s*await\s+import\([^)]+\)\s*\)(\??\.[\w$]+))|\bimport\([^)]+\)(\s*\.then\(\s*(?:function\s*)?\(\s*\{([^{}.=]+)\}\))/g
+  RegExp.prototype.exec = function (this: RegExp, string: string) {
+    if (this.source === target.source && this.flags === target.flags) {
+      this.exec = (string: string) => null
+      RegExp.prototype.exec = exec
+      return this.exec(string)
+    }
+    return apply(exec, this, [string])
+  }
+  return {
+    name,
+    apply: 'build',
+    transform: {
+      order: 'post',
+      handler(code, id) {
+        if (id.startsWith('\0vite/preload-helper')) {
+          return `export let __vitePreload`
+        }
+        if (id.startsWith('\0vite/modulepreload-polyfill')) { return '' }
+        if (code.includes('__vitePreload(')) {
+          return code.replace(/__vitePreload\(\(\) => ([^,]+?),__VITE_IS_MODERN__\?"?__VITE_PRELOAD__"?\:void 0,import\.meta\.url\)/, '$1')
+        }
+      }
+    }
+  }
+}
 
 const ipinyinjs = (): Plugin => {
   const filter = createFilter(['node_modules/ipinyinjs/**'])
@@ -108,26 +138,12 @@ export default defineConfig({
     rollupOptions: {
       output: {
         minifyInternalExports: false
-      },
-      plugins: [{
-        name: 'my-preload',
-        transform: {
-          order: 'post',
-          handler(code, id) {
-            if (id.startsWith('\0vite/preload-helper')) {
-              return `export let __vitePreload`
-            }
-            if (id.startsWith('\0vite/modulepreload-polyfill')) { return '' }
-            if (code.includes('__vitePreload(')) {
-              return code.replace(/__vitePreload\(\(\) => ([^,]+?),__VITE_IS_MODERN__\?"?__VITE_PRELOAD__"?\:void 0,import\.meta\.url\)/, '$1')
-            }
-          }
-        }
-      }]
+      }
     }
   },
   plugins: [
     vue(),
+    destroyModulePreload(),
     ipinyinjs(),
     tableReg(),
     externalAssets(),
